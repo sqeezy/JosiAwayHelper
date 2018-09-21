5f9548012dd68c3079e624195225d82a2cf07835
Anton Herzog <info@antonherzog.com>
2018-09-21 13:06:46 +0200

    Add appinsights.

--- src/JosiAwayHelper/Program.cs
+++ src/JosiAwayHelper/Program.cs
@@ -19,6 +19,7 @@ namespace JosiAwayHelper
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
